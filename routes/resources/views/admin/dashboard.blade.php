311c0d2c133ee1863b64055406dcde3f28943485
votiveneha <votivephp.neha@gmail.com>
2026-03-03 16:34:17 +0530

    3-3-2026

--- routes/resources/views/admin/dashboard.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('admin.layouts.layout')
-
-@section('content')
-<x-card-component parentHeading="Dashboard" childHeading="Dashboard" parentUrl="{{route('admin.dashboard')}}" />
-
-@endsection
--- routes/resources/views/auth/afterVerify.blade.php
+++
@@ -1,3 +0,0 @@
-@if($do==1)<h1>You Email has been verified You can <a href="{{ url('admin')}} ">Login </a> </h1>@else
-        <h1>{{ $msg }} <a href="{{ url('')}} ">Home </a> </h1>
-@endif
